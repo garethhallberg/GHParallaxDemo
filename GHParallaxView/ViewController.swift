2526c95e20527c4723198709dd616433a7328ffe
Gareth Hallberg <gareth@ghallberg.co.uk>
2020-07-14 13:30:32 +0100

    Initial Commit

---
+++ GHParallaxView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHParallaxView
+//
+//  Created by gareth15 on 14/07/2020.
+//  Copyright © 2020 gareth15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
